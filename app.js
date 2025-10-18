3e743b5ca0d0380ef46da2d3ebafea8b4cdc5779
ngoctram04 <ntntram7979@gmail.com>
2025-10-18 10:26:34 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
+const ApiError = require('./app/api-error');
 const app = express();
 
 app.use(cors());
@@ -13,4 +14,13 @@ app.get('/', (req, res) => {
 const contactRoutes = require('./app/routes/contact.route');
 app.use('/api/contacts', contactRoutes);
 
+app.use((req, res, next) => {
+
+    return next(new ApiError(404, 'Resource not found'));
+});
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error',
+    });
+});
 module.exports = app;
